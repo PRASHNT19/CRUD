2d524cbfd4e90ac2b5f9dfb058ff82382d5ef683
“Prashant <pkhatiwada157@gmail.com>
2026-01-27 21:04:10 +0545

    day 2 update(upto add edit completed feature

---
+++ js/app.js
@@ -0,0 +1,19 @@
+import { groceryItems } from "./data.js";
+import { createItems } from "./items.js";
+let items = groceryItems;
+function render() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+  const itemsElement = createItems(items);
+  app.appendChild(itemsElement);
+}
+render();
+export function editCompleted(itemId) {
+  items = items.map((item) => {
+    if (item.id === itemId) {
+      return { ...item, completed: !item.completed };
+    }
+    return item;
+  });
+  render();
+}
---
+++ js/data.js
@@ -0,0 +1,6 @@
+export const groceryItems = [
+  { id: "1", name: "milk", completed: true },
+  { id: "2", name: "bread", completed: true },
+  { id: "3", name: "Eggs", completed: true },
+  { id: "4", name: "Butter", completed: true },
+];
